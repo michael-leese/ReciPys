701562251ca0cce13fe42b2e6a3fd16cde6937fe
Michael Leese <michael-leese@outlook.com>
2019-10-05 21:00:56 +0100

    Add conifg settings and assign MongoDB values

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 #add useful dependencies
 import os
+import config
 from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+
+PWconfig = config.DBSettings['DBPassword']
+
+app.config["MONGO_DBNAME"] = 'myRecipeDB'
+app.config["MONGO_URI"] = 'mongodb+srv://root:' + PWconfig + '@recipys-giedu.mongodb.net/admin?retryWrites=true&w=majority'
 
 #Setup simple message to show server setup is working
 @app.route('/')
