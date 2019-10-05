ab0e0a07eafb814ed1e2cca961672c433b8e82bc
Michael Leese <michael-leese@outlook.com>
2019-10-05 19:37:28 +0100

    successful connection to heroku

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-
+#add useful dependencies
 import os
 from flask import Flask
 
 app = Flask(__name__)
 
-
+#Setup simple message to show server setup is working
 @app.route('/')
 def hello():
     return 'Hello ReciPys!'
@@ -13,3 +13,4 @@ if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
         debug=True)
+        # Successfully connected to heroku
