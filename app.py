035b471e3cb1cd84b52bc007338ee8f77d4c4b30
Michael Leese <michael-leese@outlook.com>
2019-10-08 21:49:09 +0100

    addmongoconfig and create hidden password variable on heroku

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 #add useful dependencies
 import os
-import config
 from flask import Flask
-from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-PWconfig = config.DBSettings['DBPassword']
+PWconfig = os.environ.get('PWMongoDB')
 
 app.config["MONGO_DBNAME"] = 'myRecipeDB'
 app.config["MONGO_URI"] = 'mongodb+srv://root:' + PWconfig + '@recipys-giedu.mongodb.net/admin?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 #Setup simple message to show server setup is working
 @app.route('/')
