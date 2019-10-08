0e2b694cc6e39bcf43f6e1d4eb5a54ae0746a4ac
Michael Leese <michael-leese@outlook.com>
2019-10-08 23:09:08 +0100

    attempt to pass mongoPW as variable

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
 #add useful dependencies
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 from bson.objectid import ObjectId
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-PWconfig = os.environ.get('PWMongoDB')
-
+PWconfig = os.environ
+PWConnectionStr = 'mongodb+srv://root:%s@recipys-giedu.mongodb.net/myRecipeDB?retryWrites=true&w=majority' % (PWconfig)
+print(PWconfig)
+print(PWConnectionStr)
 app.config["MONGO_DBNAME"] = 'myRecipeDB'
-app.config["MONGO_URI"] = 'mongodb+srv://root:' + str(PWconfig) + '@recipys-giedu.mongodb.net/admin?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = PWConnectionStr
+
 
 mongo = PyMongo(app)
 
 #Setup simple message to show server setup is working
 @app.route('/')
-def hello():
-    return 'Hello ReciPys!'
+@app.route('/get_tasks')
+def get_users():
+    return render_template("index.html", users=mongo.db.users.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
