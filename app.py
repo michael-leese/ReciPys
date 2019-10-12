571b1a454cff3ecbf2b402501ea947402182f38e
Michael Leese <michael-leese@outlook.com>
2019-10-12 11:38:46 +0100

    add templates and attempt to pass content

--- app.py
+++ app.py
@@ -14,10 +14,6 @@ app.config["MONGO_URI"] = 'mongodb+srv://root:%s@recipys-giedu.mongodb.net/myRec
 mongo = PyMongo(app)
 
 @app.route('/')
-#retrieve user
-@app.route('/get_users')
-def get_users():
-     return render_template("users.html", users=mongo.db.users.find())
 
 #retrieve recipy
 @app.route('/get_recipys')
